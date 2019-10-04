3ec1637fab86888604f408f25fdb0532554659e9
ivan <morphizm@ya.ru>
2019-10-04 13:37:45 +0300

    version

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -1,7 +1,7 @@
 # Be sure to restart your server when you modify this file.
-
+ASSET_VERSION = '0.0.1'
 # Version of your assets, change this if you want to expire all your assets.
-Rails.application.config.assets.version = '1.0.0'
+Rails.application.config.assets.version = ASSET_VERSION
 
 # Add additional assets to the asset load path.
 # Rails.application.config.assets.paths << Emoji.images_path
