0c34cc6bdd820d6b7fe966169c1de3b8d48eda55
ivan <morphizm@ya.ru>
2019-10-07 14:38:29 +0300

    fix

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
   def test_list_on_level(level)
-    users = User.where('name == ?', self.name)
+    users = User.where('name == ?', name)
     users.inject([]) do |acc, elem|
       test = Test.find(elem.test_id)
       test.level == level ? [*acc, test] : acc
