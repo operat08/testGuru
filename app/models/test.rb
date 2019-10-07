95784dc7afe668f7b332399f441cb2dd5255e211
ivan <morphizm@ya.ru>
2019-10-07 14:37:24 +0300

    fix

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,7 +1,7 @@
 class Test < ApplicationRecord
   def self.sort_by_category(category)
-    categories_id = Test.all.order(title: :desc)
-    categories_id.inject([]) do |acc, elem|
+    tests = Test.all.order(title: :desc)
+    tests.inject([]) do |acc, elem|
       current_category = Category.find(elem.category_id)
       current_category.title == category ? [*acc, elem.title] : acc
     end
