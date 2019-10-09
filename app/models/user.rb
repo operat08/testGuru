2191af416a5f934466ea4f395ca3101066ab4f28
ivan <morphizm@ya.ru>
2019-10-09 13:11:32 +0300

    fix

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   has_many :users_tests, dependent: :destroy
   has_many :tests, through: :users_tests
-  has_one :test, foreign_key: "author_id"
+  has_many :test, foreign_key: "author_id"
 
   def test_list_on_level(level)
     tests.where(level: level)
