0c7668819b46fa0eb4c75fde67aff2059dfdef5a
ivan <morphizm@ya.ru>
2019-10-09 13:21:11 +0300

    fix

--- app/models/test.rb
+++ app/models/test.rb
@@ -3,7 +3,7 @@ class Test < ApplicationRecord
   belongs_to :category
   has_many :questions, dependent: :destroy
   has_many :users_tests, dependent: :destroy
-  has_many :users, through: :users_tests
+  has_many :users, through: :users_tests, dependent: :destroy
 
   def self.sort_by_category(name)
     Category.find_by(title: name).tests.order('title desc').pluck(:title)
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   has_many :users_tests, dependent: :destroy
-  has_many :tests, through: :users_tests
-  has_many :test, foreign_key: "author_id"
+  has_many :tests, through: :users_tests, dependent: :destroy
+  has_many :test, foreign_key: "author_id", dependent: :destroy
 
   def test_list_on_level(level)
     tests.where(level: level)
