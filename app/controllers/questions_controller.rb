650f141be937687e91e9a7a2452d4f8d63504654
ivan <morphizm@ya.ru>
2019-10-12 19:10:47 +0300

    fix

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,5 +1,6 @@
 class QuestionsController < ApplicationController
   before_action :find_test
+  before_action :find_question
 
   rescue_from ActiveRecord::RecordInvalid, with: :rescue_with_question_created
   rescue_from ActiveRecord::RecordNotFound, with: :rescue_with_question_not_found
@@ -10,35 +11,39 @@ class QuestionsController < ApplicationController
   end
 
   def create
-    question = @test.questions.create!(params.require(:question).permit(:title))
+    question = @test.questions.create!(question_params)
     render html: "Question #{question.title} has been created"
   end
 
   def new; end
 
   def show
-    question_id = params['id']
-    question = @test.questions.find(question_id)
-
-    render html: question.title
+    render html: @question.title
   end
 
   def destroy
-    question_id = params['id']
-    Question.find_by_id(question_id).destroy
+    @question.destroy
   end
 
   private
 
   def find_test
-    @test = Test.find(params[:test_id])
+    @test = Test.find(params[:test_id]) if params[:test_id]
   end
 
-  def rescue_with_question_not_found
-    render plain: "Question not found"
+  def rescue_with_question_not_found(err)
+    render plain: "Question not found, #{err}"
   end
 
   def rescue_with_question_created(err)
     render plain: err.message
+  end
+
+  def find_question
+    @question = Question.find(params[:id]) if params[:id]
+  end
+
+  def question_params
+    params.require(:question).permit(:title)
   end
 end
