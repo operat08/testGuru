10511e06d88bdeaf92c6343b0cfc6e881efb2de5
ivan <morphizm@ya.ru>
2019-10-16 19:08:52 +0300

    fix

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
   has_many :created_tests, class_name: "Test", foreign_key: "author_id", dependent: :nullify
 
   validates :name, presence: true
-  validates :email, format: { with: /\w+@\w+\.\w/ }, uniqueness: true
+  validates :email, format: { with: /\w+@\w+\.\w+/ }, uniqueness: true
 
   has_secure_password
 
